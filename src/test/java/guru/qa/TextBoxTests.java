068f798eaa6bcc2fd0f998b59e4e4740d89b6ade
Kapiton Uzinsky <pervomay90@mail.ru>
2021-07-11 00:27:21 +0500

    Added build.gradle

--- src/test/java/guru/qa/TextBoxTests.java
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -4,6 +4,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -23,21 +26,32 @@ public class TextBoxTests {
         $("#userEmail").setValue("vasya@gmail.uk");
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("0102030102");
-        $("#dateOfBirthInput").click();
-        $("select.react-datepicker__month-select").selectOption("August");
-        $("select.react-datepicker__month-select").click();
-        $("select.react-datepicker__year-select").selectOption("1992");
-        $("select.react-datepicker__year-select").click();
-        $("react-datepicker__day react-datepicker__day--021").click();
+        $(("#dateOfBirthInput")).click();
+        $(".react-datepicker__month-select").selectOption("August");
+        $(".react-datepicker__year-select").selectOption("1992");
+        $((".react-datepicker__day--021")).click();
         $("#subjectsInput").setValue("QA testing");
-        $("#hobbies-checkbox-1").$(byText("Sports")).click();
-        $("#hobbies-checkbox-3").$(byText("Music")).click();
-        $("#uploadPicture").click();
-        $("#uploadPicture").val("C:\\Users\\Admin\\Desktop");
-        $("#uploadPicture").click();
-        $("#currentAddress").setValue("Lenina str, 22");
-        $("#state").$(byText("Rajasthan"));
-        $("#city").$(byText("Jaipur"));
-        $("#submit").click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/scale_1200.jpg"));
+        $("#subjectsInput").setValue("QA testing").pressEnter();
+        $("#currentAddress").setValue("address");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#submit").scrollTo().click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
+                text("Vasiliy Ivanov"),
+                text("vasya@gmail.uk"),
+                text("Male"),
+                text("0102030102"),
+                text("21 August,1992"),
+                text("QA testing"),
+                text("Sports, Music"),
+                text("scale_1200.jpg"),
+                text("address"),
+                text("NCR Delhi"));
+
     }
 }
